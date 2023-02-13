77cdcad87a35c3243da27421b6cc27df9b122e2e
Anjan Talatam <tspa2117@gmail.com>
2023-02-13 22:47:34 +0530

    📦 NEW: split logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,52 @@
+"use client";
+
 import { Inter } from "@next/font/google";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const SPLIT_AT = 700;
+
 export default function Home() {
-  return <main>Hi Split WA Status</main>;
+  const [message, setMessage] = useState("");
+  const [disabled, setDisabled] = useState(false);
+
+  const splitMessage = (msg: string) => {
+    setDisabled(true);
+
+    const len = msg.length;
+    const splits = [];
+
+    for (let i = 0; i < Math.ceil(len / SPLIT_AT); i++) {
+      const slicedMsg = msg.slice(SPLIT_AT * i, SPLIT_AT * (i + 1));
+      splits.push(slicedMsg);
+    }
+
+    console.log(splits);
+  };
+
+  return (
+    <main className="flex flex-col items-center gap-6 w-full  h-screen bg-green-50">
+      <h1 className="my-4 text-4xl text-gray-600 font-bold ">
+        split-wa-status 🔪
+      </h1>
+      <textarea
+        placeholder="Enter your message here"
+        className={`textarea textarea-lg w-[50%] ${
+          disabled ? "h-20" : "h-[50%]"
+        }`}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        // disabled={disabled}
+      />
+      <div className="badge badge-primary">Characters: {message.length}</div>
+
+      {message.length > SPLIT_AT && (
+        <button className="btn w-[50%]" onClick={() => splitMessage(message)}>
+          Split
+        </button>
+      )}
+    </main>
+  );
 }
